cb517399821e67f656a58998a7b47fa7909a8d9c
GhaithQ1 <ghaithdrh@gmail.com>
2025-02-16 18:19:49 +0300

    ..

--- js/script.js
+++ js/script.js
@@ -9,3 +9,26 @@ window.onscroll = ()=>{
     }
 }
 
+
+
+// ================================================
+
+
+document.getElementById('next').onclick = function(){
+    let lists = document.querySelectorAll('.item');
+    document.getElementById('slide').appendChild(lists[0]);
+ }
+ document.getElementById('prev').onclick = function(){
+    let lists = document.querySelectorAll('.item');
+    document.getElementById('slide').prepend(lists[lists.length - 1]);
+ }
+
+
+const mun = document.querySelector(".mun");
+
+const icon = document.querySelector('.show')
+
+icon.onclick = ()=>{
+    mun.classList.toggle('show')
+}
+
